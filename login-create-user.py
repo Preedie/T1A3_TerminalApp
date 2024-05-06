0723d162c7bf41b0847a96e5affa7dded50ca659
Preedie <kiadslawson@gmail.com>
2024-05-06 16:10:33 +1000

    Create&Signup Done (needsNotes)

--- login-create-user.py
+++ login-create-user.py
@@ -1,4 +1,4 @@
-def create_user():
+def create_user(database):
     database = open('user-database.txt','r') 
     # Database will link to the user-database.txt file and will allow the user to write login information to it. (creating their account and saving it for future sessions to login to)
     username = input('Create Username: ') 
@@ -30,61 +30,115 @@ def create_user():
         
     if password!= password_confirmation:
         print('Passwords do not match, try again')
-        create_user()
+        create_user(database)
     else:
         if len(password) <= 7:
             print('Password must be at least 7 characters')
-            create_user()
+            create_user(database)
         elif username in username_store:
             print('Username already exists, try again')
-            create_user()
+            create_user(database)
         else:
             database = open('user-database.txt','a')
             database.write(username + ',' + password + '\n')
             print(f'Welcome {username}, enjoy.')
-
-create_user()
     
-def login_user():
+def login_user(database):
     username = input('Enter Username: ') 
     password = input('Enter Password: ')
     
-    if not len(username or password)<1:
-        username_store = []
-        password_store = []
-        for  i in database:
-            a, b = i.split(',')
-            b = b.strip()
-            username_store.append(a)
-            password_store.append(b)
-        data = dict(zip(username_store, password_store))
+    if not username or not password:
+        print('Username or Password cannot be empty')
+        return login_user(database)
     
-        try:
-            if data[username]:
-                try:
-                    if password == data[username]:
-                        print('Login successful')
-                        print('Hi, ', username)
-                    else:
-                        print('Login failed Username or Password incorrect')
-                except:
-                    print('Login failed Username or Password incorrect')
-            else:
-                    print('Username doesnt Exist')
-        except:
-            print('Login Error')
+    for line in database:
+        uname, pwd = line.strip().split(',')
+        if username == uname and password == pwd:
+            print('Login successful')
+            print('Hi,', username)
+            return
+    print('Login failed: Username or Password incorrect')
+    
 
-def home(option=None):
+def home(database):
     option = input('Login | Signup: ')
-    if option == 'login':
-        login_user()
-    elif:
-        option == 'Signup'
-        create_user()
+    if option == 'Login':
+        login_user(database)
+    elif option == 'Signup':
+        create_user(database)
     else:
         print('Please enter an option')
-home()       
+      
         
+
+
+
+
+
+def add_task(): 
+    print('Add task Function')
+    #This function is used to add the task.
+    
+def created_task_info():
+    input('Enter Task Information: ')
+    # This function is used to enter created tasks information.
         
+def view_all_tasks():
+    print('View all tasks Function')
+    # This function is used to view all the tasks created.
+    
+def completed_tasts():
+    print('Mark a task as complete Function')
+    # This function is used to mark a task as complete.
+    
+def delete_task():
+    print('Delete a task Function')
+    # This function is used to delete tasks and send them to the ARCHIVED function
+
+    
+def help_controls():
+    print('Add decription of how to display and navigate menus create/del tasks')
+    # This function will show the user how to use the application
+    
+def archived_tasks():
+    print('Archived tasks Function')
+    # This function is used to view all the archived tasks and allow the user to un archive.
+
+
+def main_menu():
+
+    options = {
+    '1': add_task,
+    '2': view_all_tasks,
+    '3': completed_tasts,
+    '4': delete_task,
+    '5': help_controls,
+    '6': archived_tasks, 
+    }
+
+    while True:
+        print('Welcome to To Do List')
+        print('1. Add a tasks')
+        print('2. View all tasks')
+        print('3. Completed tasks')
+        print('4. Delete tasks')
+        print('5. Help and Controls')
+        print('6. Archived tasks')
+        print('7. Logout of application')
+        choice = input('Enter Menu number: ')
+        
+        if choice in options:
+            options[choice]()
+        else:
+            print('Please enter a valid menu number')
+    
+def main():
+    with open('user-database.txt', 'r') as file:
+        home(file)
+
+if __name__ == "__main__":
+    main()  
+
+main_menu() 
         
         
